57f7d0a35853cc11b48a3634fdd49d22ee0af399
Biohazardx44 <nikolailievski420@gmail.com>
2023-05-19 19:26:08 +0200

    Tables in SQL

--- Homework1/Homework1.sql
+++ Homework1/Homework1.sql
@@ -3,55 +3,55 @@ CREATE DATABASE SEDCACADEMYDB
 USE SEDCACADEMYDB
 
 CREATE TABLE dbo.Student (
-    Id INT PRIMARY KEY IDENTITY(1,1),
-    FirstName VARCHAR(200),
-    LastName VARCHAR(200),
-    DateOfBirth DATE,
-    EnrolledDate DATE,
-    Gender VARCHAR(10),
-    NationalIDNumber INT,
-    StudentCardNumber SMALLINT
+    Id INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+    FirstName VARCHAR(200) NOT NULL,
+    LastName VARCHAR(200) NOT NULL,
+    DateOfBirth DATE NOT NULL,
+    EnrolledDate DATE NOT NULL,
+    Gender VARCHAR(10) NOT NULL,
+    NationalIDNumber INT NOT NULL,
+    StudentCardNumber SMALLINT NOT NULL
 )
 
 CREATE TABLE dbo.Teacher (
-    Id INT PRIMARY KEY IDENTITY(1,1),
-    FirstName VARCHAR(200),
-    LastName VARCHAR(200),
-    DateOfBirth DATE,
-    AcademicRank VARCHAR(100),
-    HireDate DATE
+    Id INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+    FirstName VARCHAR(200) NOT NULL,
+    LastName VARCHAR(200) NOT NULL,
+    DateOfBirth DATE NOT NULL,
+    AcademicRank VARCHAR(100) NULL,
+    HireDate DATE NOT NULL
 )
 
 CREATE TABLE dbo.Grade (
-    Id INT PRIMARY KEY IDENTITY(1,1),
-    StudentID SMALLINT,
-    CourseID SMALLINT,
-    TeacherID SMALLINT,
-    Grade VARCHAR(10),
-    Comment VARCHAR(500),
-    CreatedDate DATE
+    Id INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+    StudentID SMALLINT NOT NULL,
+    CourseID SMALLINT NOT NULL,
+    TeacherID SMALLINT NOT NULL,
+    Grade VARCHAR(10) NULL,
+    Comment VARCHAR(500) NULL,
+    CreatedDate DATE NULL
 )
 
 CREATE TABLE dbo.Course (
-    Id INT PRIMARY KEY IDENTITY(1,1),
-    [Name] VARCHAR(200),
-    Credit SMALLINT,
-    AcademicYear SMALLINT,
-    Semester VARCHAR(100)
+    Id INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+    [Name] VARCHAR(200) NOT NULL,
+    Credit SMALLINT NULL,
+    AcademicYear SMALLINT NOT NULL,
+    Semester VARCHAR(100) NULL
 )
 
 CREATE TABLE dbo.GradeDetails (
-    Id INT PRIMARY KEY IDENTITY(1,1),
-    GradeID SMALLINT,
-    AchievementTypeID SMALLINT,
-    AchievementPoints TINYINT,
-    AchievementMaxPoints TINYINT,
-    AchievementDate DATE
+    Id INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+    GradeID SMALLINT NOT NULL,
+    AchievementTypeID SMALLINT NOT NULL,
+    AchievementPoints TINYINT NULL,
+    AchievementMaxPoints TINYINT NULL,
+    AchievementDate DATE NULL
 )
 
 CREATE TABLE dbo.AchievementType (
-    Id INT PRIMARY KEY IDENTITY(1,1),
-    [Name] VARCHAR(200),
-    [Description] VARCHAR(500),
-    ParticipationRate DECIMAL
+    Id INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+    [Name] VARCHAR(200) NOT NULL,
+    [Description] VARCHAR(500) NULL,
+    ParticipationRate DECIMAL NULL
 )
